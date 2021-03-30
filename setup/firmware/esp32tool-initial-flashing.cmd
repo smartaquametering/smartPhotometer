81d54a7fcfe2d1d7275095ca6d417dbc8e8bdf65
Joerg Heinemann <heinemannj66@gmail.com>
2021-03-30 11:44:57 +0200

    Initial commit

--- setup/firmware/esp32tool-initial-flashing.cmd
+++ setup/firmware/esp32tool-initial-flashing.cmd
@@ -1,6 +1,6 @@
 
 REM erase
-esp32tool.exe --chip esp32 --port COM8 --baud 256000 --before default_reset --after hard_reset erase_flash
+esp32tool.exe --chip esp32 --port COM4 --baud 256000 --before default_reset --after hard_reset erase_flash
 
 REM flash ESP_Easy_mega_20201102_test_ESP32_4M316k-factory.bin
-esp32tool.exe --chip esp32 --port COM8 --baud 256000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x0 ./bin/ESP_Easy_mega_20201102_test_ESP32_4M316k-factory.bin
+esp32tool.exe --chip esp32 --port COM4 --baud 256000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x0 ./bin/ESP_Easy_mega_20201102_test_ESP32_4M316k-factory.bin
